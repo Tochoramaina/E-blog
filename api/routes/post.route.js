cdc5a50b1441b69ddb04603ba1c3b5324cd4f023
Samwel Tochora <samtochora2001@gmail.com>
2025-04-08 09:56:29 +0300

    add create post api route

---
+++ api/routes/post.route.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { verifyToken } from '../utils/verifyUser';
+import { create } from '../controllers/post.controller';
+
+const router = express.Router();
+
+router.post('/create', verifyToken, create)

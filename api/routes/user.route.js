67093d37116e5f2f30c203c367ef0bce0032c71a
Samwel Tochora <samtochora2001@gmail.com>
2025-04-11 12:57:44 +0300

    add delete user functionality to the admin user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -8,5 +8,5 @@ router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout)
-router.get('/getusers', verifyToken, getUsers)
+router.get('/getusers', verifyToken, getUsers);
 export default router; 

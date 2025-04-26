eb3ed96bcd168b240032b640bc91f15bbef225a1
Samwel Tochora <samtochora2001@gmail.com>
2025-04-26 09:48:12 +0300

    complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {Link, useLocation, useNavigate} from 'react-router-dom'
 import { Navbar, TextInput, Button, Dropdown, Avatar} from 'flowbite-react';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon, FaSun} from "react-icons/fa"
@@ -7,34 +7,52 @@ import {useSelector} from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/User/userSlice';
+
 const Header = () => {
     const path = useLocation().pathname;
     const dispatch = useDispatch()
     const {currentUser} = useSelector((state) => state.user)
     const {theme} = useSelector((state) => state.theme)
-      const handleSignOut = async () => {
-        try {
-          const res = await fetch('/api/user/signout', {
-            method : "POST"
-          });
-          const data = await res.json();
-          if(!res.ok){
-            console.log(data.message)
-          }
-          else{
-            dispatch(signoutSuccess())
-          }
-        } catch (error) {
-          console.log(error.message)
-        }
+    const [searchTerm, setSearchTerm] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if(searchTermFromUrl){
+        setSearchTerm(searchTermFromUrl)
       }
+    }, [location.search])
+    const handleSignOut = async() => {
+      try {
+        const res = await fetch('/api/user/signout', {
+          method : "POST"
+        });
+        const data = await res.json();
+        if(!res.ok){
+          console.log(data.message)
+        }
+        else{
+          dispatch(signoutSuccess())
+        }
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const urlParams = new URLSearchParams(location.search);
+      urlParams.set('searchTerm', searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`)
+    }
   return (
     <Navbar className="border-b-2">
         <Link to= "/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Maina's</span>Blog
         </Link>
-        <form>
-            <TextInput type='text' placeholder='Search...'  className='hidden lg:inline'/>
+        <form onSubmit={handleSubmit}>
+            <TextInput type='text' placeholder='Search...'  className='hidden lg:inline' rightIcon={AiOutlineSearch} value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
         </form>
        <Button className='w-12 h-10 lg:hidden' color='gray' pill><AiOutlineSearch/></Button>
        <div className='flex gap-2 md:order-2'>

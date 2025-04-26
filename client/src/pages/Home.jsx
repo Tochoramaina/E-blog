6e292730de98acdd978f78653dfdf02cc480bc64
Samwel Tochora <samtochora2001@gmail.com>
2025-04-26 10:25:42 +0300

    xxx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,8 +18,8 @@ const Home = () => {
   }, [])
   return (
     <div>
-      <div className="flex flex-col gap-6 lg:p-18 px-3 max-w-6xl mx-auto">
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+      <div className="flex flex-col gap-6 lg:p-28 px-3 max-w-6xl mx-auto">
+        <h1 className='text-3xl font-bold lg:text-5xl'>Welcome to my Blog</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo omnis accusamus quia aut similique soluta asperiores rerum cumque suscipit aliquam accusantium eius odio expedita quasi in dolorum, impedit labore voluptas! 
         </p>
         <Link to={'search'} className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
